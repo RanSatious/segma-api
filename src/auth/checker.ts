78d8c4133543c6af8c73924e5d80e1a817eee6ec
rxm <383902558@qq.com>
2020-06-30 15:00:22 +0800

    feat: add checker

---
+++ src/auth/checker.ts
@@ -0,0 +1,42 @@
+import { setToken, getToken } from 'src/token/token';
+import { logout } from 'src/api/message';
+
+interface IAuthConfig {
+    autoLogout: boolean;
+}
+
+function AuthChecker(config: IAuthConfig = { autoLogout: true }) {
+    return {
+        watch: {
+            $route: {
+                immediate: true,
+                handler(val: any) {
+                    if (!val.name) {
+                        return;
+                    }
+                    let { token } = val.query;
+                    if (!token) {
+                        token = new URLSearchParams(location.search).get('token');
+                    }
+                    setToken(token);
+                    if (token) {
+                        let { token: tk, ...query } = (this as any).$route.query;
+                        (this as any).$router.push({
+                            name: (this as any).$route.name,
+                            query,
+                        });
+                    }
+                },
+            },
+        },
+        async mounted() {
+            const { autoLogout } = config;
+            let token = await getToken();
+            if (!token && autoLogout) {
+                logout();
+            }
+        },
+    };
+}
+
+export default AuthChecker;
